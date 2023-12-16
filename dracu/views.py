42aa466db8b0c770e2e5f13f6258e37fb8ec497c
victor.teixido <victor.teixido@est.fib.upc.edu>
2023-12-16 14:13:05 +0100

    NEWS: Removed accent from news title

--- dracu/views.py
+++ dracu/views.py
@@ -5,15 +5,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-news_dict = {0: {"title": "La Marató 2023",
+news_dict = {0: {"title": "La Marato 2023",
                  "link": "https://www.ccma.cat/tv3/marato/",
-                 "img": "img1.png"},
-             1: {"title": "Las farmacias catalanas distribuirán productos menstruales gratuitos a partir de 2024",
+                 "img": "https://pessebre.org/wp-content/uploads/2022/12/logo-lamarato_normal.jpg"},
+             1: {"title": "Las farmacias catalanas distribuiran productos menstruales gratuitos a partir de 2024",
                  "link": "https://elpais.com/espana/catalunya/2023-09-21/las-farmacias-catalanas-distribuiran-productos-menstruales-gratuitos-a-partir-de-2024.html#:~:text=Las%20mujeres%20catalanas%20tendr%C3%A1n%20derecho,del%20primer%20trimestre%20de%202024.",
-                 "img": "img2.png"},
-             2: {"title": "Cómo ayudar a tu hija a superar el miedo al uso del tampón y la copa menstrual",
+                 "img": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ1ARS6bM_n8mOlH2UjPFqSaNIxyqLSEnueI2B3-otT3VU_TRR7"},
+             2: {"title": "Como ayudar a tu hija a superar el miedo al uso del tampon y la copa menstrual",
                  "link": "https://elpais.com/mamas-papas/expertos/2023-08-28/como-ayudar-a-tu-hija-a-superar-el-miedo-al-uso-del-tampon-y-la-copa-menstrual.html",
-                 "img": "img3.png"}}
+                 "img": "https://imagenes.elpais.com/resizer/uuGrWL3N7hGNPSKoNGjn49DHY5Q=/1200x0/filters:focal(2340x1430:2350x1440)/cloudfront-eu-central-1.images.arcpublishing.com/prisa/FG7RYWHKMBFGDOU2E4SX5CWAME.jpg"}}
 
 
 class HealthCheckApiView(APIView):

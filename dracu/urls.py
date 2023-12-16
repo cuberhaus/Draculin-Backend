da05a3107f347d9a24154e2770df2e3fef692853
victor.teixido <victor.teixido@est.fib.upc.edu>
2023-12-16 12:21:53 +0100

    BACKEND: Added {title, link, img} to news endpoint

--- dracu/urls.py
+++ dracu/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import NewsApiView, QuizApiView, HealthCheckApiView, ChatApiView, CameraApiView
 
 urlpatterns = [
-    path('healthcheck', HealthCheckApiView.as_view(), name='healthcheck'),
+    path('healthcheck/', HealthCheckApiView.as_view(), name='healthcheck'),
     path('news/', NewsApiView.as_view(), name='news'),
     path('quiz', QuizApiView.as_view(), name='quiz'),
-    path('news/', ChatApiView.as_view(), name='news'),
-    path('quiz', CameraApiView.as_view(), name='quiz')
+    path('chat/', ChatApiView.as_view(), name='chat'),
+    path('camera/', CameraApiView.as_view(), name='camera')
 ]
--- dracu/views.py
+++ dracu/views.py
@@ -5,9 +5,15 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-news_dict = {0: "BitsxlaMarato 2023",
-             1: "Las farmacias catalanas distribuirán productos menstruales gratuitos a partir de 2024",
-             2: "Draculito"}
+news_dict = {0: {"title": "La Marató 2023",
+                 "link": "https://www.ccma.cat/tv3/marato/",
+                 "img": "img1.png"},
+             1: {"title": "Las farmacias catalanas distribuirán productos menstruales gratuitos a partir de 2024",
+                 "link": "https://elpais.com/espana/catalunya/2023-09-21/las-farmacias-catalanas-distribuiran-productos-menstruales-gratuitos-a-partir-de-2024.html#:~:text=Las%20mujeres%20catalanas%20tendr%C3%A1n%20derecho,del%20primer%20trimestre%20de%202024.",
+                 "img": "img2.png"},
+             2: {"title": "Cómo ayudar a tu hija a superar el miedo al uso del tampón y la copa menstrual",
+                 "link": "https://elpais.com/mamas-papas/expertos/2023-08-28/como-ayudar-a-tu-hija-a-superar-el-miedo-al-uso-del-tampon-y-la-copa-menstrual.html",
+                 "img": "img3.png"}}
 
 
 class HealthCheckApiView(APIView):
@@ -29,10 +35,10 @@ class QuizApiView(APIView):
 class ChatApiView(APIView):
 
     def get(self, request):
-        return Response({"Chat": news_dict}, status=status.HTTP_200_OK)
+        return Response({"Chat": "chat"}, status=status.HTTP_200_OK)
 
 
 class CameraApiView(APIView):
 
     def get(self, request):
-        return Response({"Camera": news_dict}, status=status.HTTP_200_OK)
+        return Response({"Camera": "camera"}, status=status.HTTP_200_OK)

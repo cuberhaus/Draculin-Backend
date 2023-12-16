e688bace81447f746f843f61a467042db20626a1
victor.teixido <victor.teixido@est.fib.upc.edu>
2023-12-16 11:53:29 +0100

    BACKEND: HealthCheck, Quiz, Chat and Camera endpoints added

--- dracu/urls.py
+++ dracu/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import NewsApiView
+from .views import NewsApiView, QuizApiView, HealthCheckApiView, ChatApiView, CameraApiView
 
 urlpatterns = [
-    path('news/',NewsApiView.as_view(),name='news'),
+    path('healthcheck', HealthCheckApiView.as_view(), name='healthcheck'),
+    path('news/', NewsApiView.as_view(), name='news'),
+    path('quiz', QuizApiView.as_view(), name='quiz'),
+    path('news/', ChatApiView.as_view(), name='news'),
+    path('quiz', CameraApiView.as_view(), name='quiz')
 ]
--- dracu/views.py
+++ dracu/views.py
@@ -5,6 +5,32 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+news_dict = {0: "new0", 1: "new1", 2: "new2"}
+
+
+class HealthCheckApiView(APIView):
+    def get(self, request):
+        return Response("Health Check", status=status.HTTP_200_OK)
+
+
 class NewsApiView(APIView):
-    def get(self,request):
-        return Response({"message":"Test"},status=status.HTTP_200_OK)
+    def get(self, request):
+        return Response({"news": news_dict}, status=status.HTTP_200_OK)
+
+
+class QuizApiView(APIView):
+
+    def get(self, request):
+        return Response("Quiz", status=status.HTTP_200_OK)
+
+
+class ChatApiView(APIView):
+
+    def get(self, request):
+        return Response({"Chat": news_dict}, status=status.HTTP_200_OK)
+
+
+class CameraApiView(APIView):
+
+    def get(self, request):
+        return Response({"Camera": news_dict}, status=status.HTTP_200_OK)
